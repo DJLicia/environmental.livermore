6f90cb30f7bcd8ad071cb1f572cc76d2ebfbd959
Alicia Zhang <eishazhang01@gmail.com>
2024-12-11 00:19:11 -0500

    Update maps.js
    Fixed map toggle button functionality

--- maps.js
+++ maps.js
@@ -10,44 +10,56 @@
 
 (function() {
     "use strict"
+
+    window.addEventListener('load', init);
+
     function init() {
         const mapImages = qsa(".overlayImg");
 
         mapImages.forEach((img) => {
             img.addEventListener("click", () => toggleOpacity(img));
         });
+    
+        setupMapButton('calEnviroButton', 'calEnviroScreen');
+        setupMapButton('lowIncomeButton', 'lowIncome');
+        setupMapButton('limitedEnglishButton', 'limitedEnglish');
+    }
 
-        document.getElementById('calEnviroButton').addEventListener('click', () => {
-            switchMap('calEnviroScreen');
-        });
-        document.getElementById('lowIncomeButton').addEventListener('click', () => {
-            switchMap('lowIncome');
-        });
-        document.getElementById('limitedEnglishButton').addEventListener('click', () => {
-            switchMap('limitedEnglish');
-        });
+    /**
+     * Helper to set up map button event listeners
+     * @param {string} buttonId - ID of the button element
+     * @param {string} mapId - ID of the corresponding map element
+     */
+    function setupMapButton(buttonId, mapId) {
+        const button = document.getElementById(buttonId);
+        if (button) {
+            button.addEventListener('click', () => switchMap(mapId));
+        } else {
+            console.warn(`Button of ID ${buttonId} not found!!`);
+        }
     }
 
     function toggleOpacity(image) {
         image.style.opacity = image.style.opacity === "1" ? "0.5" : "1";
     }
 
+    /**
+     * switch between maps
+     * @param {string} mapId - ID of the map to show
+     */
     function switchMap(mapId) {
         const maps = qsa('.overlayImg');
-        maps.forEach(map => map.classList.add('hidden'));
-        document.getElementById(mapId).classList.remove('hidden');
-    }
-
-    function switchParagraph(paragraphId) {
-        const paragraph = document.getElementById(paragraphId);
-
-        // Toggle visibility by switching 'hidden' class
-        if (paragraph.classList.contains('hidden')) {
-            paragraph.classList.remove('hidden');
-        } else {
-            paragraph.classList.add('hidden');
-        }
-    }
-    
-    init();
-});
+        maps.forEach(map => {
+            map.classList.add('hidden');
+            // Reset opacity when hiding
+            map.style.opacity = "1";
+        });
+      // show selected map
+      const targetMap = document.getElementById(mapId);
+      if (targetMap) {
+          targetMap.classList.remove('hidden');
+      } else {
+          console.warn(`Map with id ${mapId} not found`);
+      }
+  }
+})();
